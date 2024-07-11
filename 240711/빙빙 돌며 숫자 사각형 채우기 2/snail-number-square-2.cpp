5f3290e21fa25bbe248359c51101dc0869464374
honey3491 <65853514+honey3491@users.noreply.github.com>
2024-07-11 16:56:29 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240711/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, value[100][100] = { { 0, } }, now[2] = { 0, 0 }, move[4][2] = { {1, 0} ,{0, 1}, {-1, 0}, {0, -1} };
+	int move_val = 0;
+
+	cin >> n >> m;
+	for (int i = 0; i < n * m; i++)
+	{
+		value[now[0]][now[1]] = i+1;
+		if ((now[1] == m - 1 && move_val == 1) || (now[0] == n - 1 && move_val == 0)|| (now[1] == 0 && move_val == 3) || (now[0] == 0 && move_val == 2) || value[now[0] + move[move_val][0]][now[1] + move[move_val][1]] != 0)
+			move_val = (move_val + 1) % 4;
+
+		now[0] += move[move_val][0];
+		now[1] += move[move_val][1];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+			cout << value[i][j] << ' ';
+		cout << '\n';
+	}
+}
