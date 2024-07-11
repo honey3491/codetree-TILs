e21c0bd48510258155025f73e876700b531b3202
honey3491 <65853514+honey3491@users.noreply.github.com>
2024-07-11 19:23:37 +0900

    Title: 최고의 33위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240711/최고의 33위치/best-place-of-33.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int map[20][20];
+
+int get_gold(int r, int c)
+{
+	int count = 0;
+	for (int i = r; i < r + 3; i++)
+	{
+		for (int j = c; j < c + 3; j++)
+			count += map[r + i][c + j];
+	}
+	return count;
+}
+
+int main()
+{
+	int N, max_gold = -1;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cin >> map[i][j];
+		}
+	}
+
+	for (int i = 0; i < N - 2; i++)
+	{
+		for (int j = 0; j < N - 2; j++)
+			max_gold = max(get_gold(i, j), max_gold);
+	}
+	cout << max_gold;
+	return 0;
+}
