98309e624267147282e2734060f139a3e9e89b1f
honey3491 <65853514+honey3491@users.noreply.github.com>
2024-07-11 17:31:34 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240711/빙빙 돌며 사각형 채우기/snail-alphabet-square.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, now[2] = { 0, 0 }, move[4][2] = { {0, 1} ,{1, 0}, {0, -1}, {-1, 0} };
+	char value[100][100] = { { 0, } };
+	int move_val = 0;
+
+	cin >> n >> m;
+	char input = 'A';
+	for (int i = 0; i < n * m; i++)
+	{
+		value[now[0]][now[1]] = input++;
+		if ((now[1] == m - 1 && move_val == 0) || (now[0] == n - 1 && move_val == 1 )|| (now[1] == 0 && move_val == 2) || (now[0] == 0 && move_val == 3) || value[now[0] + move[move_val][0]][now[1] + move[move_val][1]] != 0)
+			move_val = (move_val + 1) % 4;
+
+		now[0] += move[move_val][0];
+		now[1] += move[move_val][1];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+			cout << value[i][j] << ' ';
+		cout << '\n';
+	}
+}
