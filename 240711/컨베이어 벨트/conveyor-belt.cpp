1557f103da4238d5d5312427b81237288e44697e
honey3491 <65853514+honey3491@users.noreply.github.com>
2024-07-11 20:07:12 +0900

    Title: 컨베이어 벨트, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240711/컨베이어 벨트/conveyor-belt.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <deque>
+using namespace std;
+
+
+int main()
+{
+	short n, t, a;
+	deque <short> v;
+	cin >> n >> t;
+	
+	for (int i = 0; i < n*2; i++)
+	{
+		cin >> a;
+		v.push_back(a);
+	}
+	
+	t = t % (n * 2);
+
+	for (int i = 0; i < t; i++)
+	{
+		short tmp = v.back();
+		v.pop_back();
+		v.push_front(tmp);
+	}
+	
+	for (int i = 0; i < n; i++)
+		cout << v[i] << ' ';
+	cout << '\n';
+	for (int i = n; i < n * 2; i++)
+		cout << v[i] << ' ';
+
+}
