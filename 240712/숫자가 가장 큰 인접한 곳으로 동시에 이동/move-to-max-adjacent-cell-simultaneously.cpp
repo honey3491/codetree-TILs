1446435bbdeca546335b044ed73d8e5c979f9b85
honey3491 <65853514+honey3491@users.noreply.github.com>
2024-07-12 19:26:01 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240712/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.cpp
+++ 240712/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <algorithm>
-#include <deque>
 
 using namespace std;
 
-short n, m, table[20][20], balls[20][20] = {0, }, tmp[20][20] = {0, }, dxdy[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}, t;
+int n, m, table[20][20], balls[20][20] = { 0, }, tmp[20][20] = { 0, }, dxdy[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} }, t, c = 0;
 
 bool can_move(int r, int c)
 {
@@ -16,32 +15,33 @@ bool can_move(int r, int c)
 
 void remove_duplication()
 {
+	c = 0;
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
 			if (tmp[i][j] >= 2)
 			{
+				m -= tmp[i][j];
 				tmp[i][j] = 0;
-				m -= 2;
 			}
 			balls[i][j] = tmp[i][j];
+			c += balls[i][j];
 			tmp[i][j] = 0;
 		}
 	}
-	
 }
 
 void move_to_maximum(int r, int c)
 {
-	short maximum = 0, max_r, max_c;
+	short maximum = -1, max_r, max_c;
 	for (int i = 0; i < 4; i++)
 	{
-		if (can_move(r + dxdy[i][0], c + dxdy[i][1]))
+		if (can_move((r + dxdy[i][0]), (c + dxdy[i][1])))
 		{
 			if (maximum < table[r + dxdy[i][0]][c + dxdy[i][1]])
 			{
-				maximum = table[r + dxdy[i][0]][c+dxdy[i][1]];
+				maximum = table[r + dxdy[i][0]][c + dxdy[i][1]];
 				max_r = r + dxdy[i][0];
 				max_c = c + dxdy[i][1];
 			}
@@ -53,16 +53,14 @@ void move_to_maximum(int r, int c)
 
 void simulate()
 {
-	
-	for (int j = 0; j < n; j++)
+	for (int i = 0; i < n; i++)
 	{
-		for (int k = 0; k < n; k++)
+		for (int j = 0; j < n; j++)
 		{
-			if (balls[j][k] != 0) 
-				move_to_maximum(j, k);
+			if (balls[i][j] != 0)
+				move_to_maximum(i, j);
 		}
 	}
-	
 	remove_duplication();
 }
 
@@ -74,16 +72,18 @@ int main()
 		for (int j = 0; j < n; j++)
 			cin >> table[i][j];
 	}
+
 	for (int i = 0; i < m; i++)
 	{
 		short r, c;
 		cin >> r >> c;
-		balls[r-1][c-1]++;
+		balls[r - 1][c - 1]++;
 	}
 
 	for (int i = 0; i < t; i++)
 	{
 		simulate();
 	}
-	cout << m;
+
+	cout << c;
 }
