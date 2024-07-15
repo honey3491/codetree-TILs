a8c5e4bb5bf04e114ca7738063b9c5adb1f519b0
honey3491 <65853514+honey3491@users.noreply.github.com>
2024-07-15 20:39:24 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 95ms, Memory: 1MB, Status: Passed - CodeTree

--- 240715/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.cpp
+++ 240715/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.cpp
@@ -1,41 +1,37 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
+
 using namespace std;
 
-int N, K;
+int k, n;
+vector<int> selected_nums;
 
-void print_num(int a)
-{
-    if (a == pow(K, N))
-        exit(0);
-    int tmp = a, arr[8] = { 0, };
-    for (int i = N - 1; i >= 0; i--)
-    {
-        arr[N-i-1] = tmp / int(pow(K, i)) + 1;
-        tmp = tmp % int(pow(K, i));
-    }
-    if (K >= 3)
-    {
-        for (int i = 2; i < N; i++)
-        {
-            if (arr[i] == arr[i - 1] && arr[i] == arr[i - 2])
-            {
-                print_num(a + 1);
-                exit(0);
-            }
-        }
-    }
-
-    for (int i = 0; i < N; i++)
-        cout << arr[i] << ' ';
-    cout << '\n';
-
-    print_num(a + 1);
+void PrintPermutation() {
+    for (int i = 0; i < (int)selected_nums.size(); i++)
+        cout << selected_nums[i] << " ";
+    cout << endl;
 }
+
+void FindPermutations(int cnt) {
+    if (cnt == n) {
+        PrintPermutation();
+        return;
+    }
+
+    for (int i = 1; i <= k; i++) {
+        if (cnt >= 2 && i == selected_nums[cnt - 1] && i == selected_nums[cnt - 2])
+            continue;
+
+        selected_nums.push_back(i);
+        FindPermutations(cnt + 1);
+        selected_nums.pop_back();
+
+    }
+}
+
 int main() {
+    cin >> k >> n;
 
-    cin >> K >> N;
-    print_num(0);
-
+    FindPermutations(0);
     return 0;
 }
