faa9f512307131dd66a15810078d9249c0f15721
honey3491 <65853514+honey3491@users.noreply.github.com>
2024-07-15 20:26:18 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240715/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int N, K;
+
+void print_num(int a)
+{
+    if (a == pow(K, N) - 1)
+        exit(1);
+    int tmp = a, arr[8] = { 0, };
+    for (int i = N - 1; i >= 0; i--)
+    {
+        arr[N-i-1] = tmp / int(pow(K, i)) + 1;
+        tmp = tmp % int(pow(K, i));
+    }
+    if (K >= 3)
+    {
+        for (int i = 2; i < N; i++)
+        {
+            if (arr[i] == arr[i - 1] && arr[i] == arr[i - 2])
+            {
+                print_num(a + 1);
+                exit(1);
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+        cout << arr[i] << ' ';
+    cout << '\n';
+
+    print_num(a + 1);
+}
+int main() {
+
+    cin >> K >> N;
+    print_num(0);
+    return 0;
+}
