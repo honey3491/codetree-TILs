86ff7332b2d917dc8e86d2e422dad11b777c9748
honey3491 <65853514+honey3491@users.noreply.github.com>
2024-07-23 05:33:13 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240723/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
+++ 240723/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -1,28 +1,61 @@
 #include <iostream>
 using namespace std;
 
+bool result = false;
+int n, m;
+short grid[100][100], visited[100][100] = { 0, };
+
 bool InRange(int x, int y)
 {
-
-}
-bool CanGo(int x, int y){
-    if(!InRange(x, y))
+    return x >= 0 && x < n && y >= 0 && y < m;
         
+}
+
+bool CanGo(int x, int y) {
+    if (!InRange(x, y))
+        return false;
+    if (visited[x][y] || grid[x][y] == 0)
+        return false;
+    return true;
 
 }
 void DFS(int x, int y)
 {
-    int dx[2] = {1, 0};
-    int dy[2] = {0, 1};
 
-    for(int i = 0 ; i< 2; i++)
+    if (x == n-1 && y == m-1)
     {
-        int new_x = x + dx[i];
-
+        cout << 1;
+        result = true;
+        return;
     }
 
+    int dx[2] = { 1, 0 };
+    int dy[2] = { 0, 1 };
+
+
+    for (int i = 0; i < 2; i++)
+    {
+        int new_x = x + dx[i];
+        int new_y = y + dy[i];
+        if (CanGo(new_x, new_y))
+        {
+            visited[new_x][new_y] = 1;
+            DFS(new_x, new_y);
+        }
+    }
+    return;
 }
 int main() {
-    // 여기에 코드를 작성해주세요.
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> grid[i][j];
+        }
+    }
+    DFS(0, 0);
+    if (!result)
+        cout << 0;
     return 0;
 }
