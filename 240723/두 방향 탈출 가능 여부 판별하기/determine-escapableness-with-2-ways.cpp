76ae0e8c4652b179bf28dc9642ab036437e865c0
honey3491 <65853514+honey3491@users.noreply.github.com>
2024-07-23 05:33:02 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240723/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool InRange(int x, int y)
+{
+
+}
+bool CanGo(int x, int y){
+    if(!InRange(x, y))
+        
+
+}
+void DFS(int x, int y)
+{
+    int dx[2] = {1, 0};
+    int dy[2] = {0, 1};
+
+    for(int i = 0 ; i< 2; i++)
+    {
+        int new_x = x + dx[i];
+
+    }
+
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
