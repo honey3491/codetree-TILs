ebe8a0fc97bac26a6f083c4df705c109ed5b314c
honey3491 <65853514+honey3491@users.noreply.github.com>
2024-07-25 09:12:04 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240725/계단 오르기/climbing-stairs.cpp
+++ 240725/계단 오르기/climbing-stairs.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 
 int count = 0;
-int memo[1000] = {0, };
+long long memo[1000] = {0, };
 
 int stairs(int n)
 {
-    if(n < 0)
+    if(n < 0){
         return 0;
-    if(memo[n] != 0)
+    }
+    if(memo[n] != 0){
         return memo[n];
+    }
     else
         memo[n] = stairs(n-2) + stairs(n-3);
     return memo[n];
@@ -20,7 +22,7 @@ int main() {
     memo[2] = 1;
     memo[3] = 1;
     cin >> num;
-    count = stairs(num);
+    count = stairs(num) % 10007;
     cout << count;
     return 0;
 }
