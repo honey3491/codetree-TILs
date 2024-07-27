26921b255c31ab7bef683c6c46d41b75d4a04197
honey3491 <65853514+honey3491@users.noreply.github.com>
2024-07-27 18:14:20 +0900

    Title: 편집거리 3, Time: 11ms, Memory: 3MB, Status: Passed - CodeTree

--- 240727/편집거리 3/edit-distance-3.cpp
+++ 240727/편집거리 3/edit-distance-3.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <algorithm> // max 함수를 사용하기 위해 추가
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -14,6 +14,7 @@ int main()
     int b_len = B_string.length();
 
     int** count_string = new int* [a_len + 1];
+
     for (int i = 0; i <= a_len; i++)
     {
         count_string[i] = new int[b_len + 1];
@@ -23,22 +24,30 @@ int main()
     {
         for (int j = 0; j <= b_len; j++)
         {
-            count_string[i][j] = 0;
+            count_string[i][j] = 1000000;
         }
     }
 
-    for (int i = 1; i <= a_len; i++)
-    {
-        for (int j = 1; j <= b_len; j++)
-        {
-            if (A_string[i - 1] == B_string[j - 1])
-                count_string[i][j] = count_string[i - 1][j - 1] ;
-            else
-                count_string[i][j] = min(count_string[i][j - 1], count_string[i - 1][j]) + 2;
+    for (int i = 1; i <= a_len; ++i) {
+        count_string[i][0] = i;
+    }
+
+    for (int j = 1; j <= b_len; ++j) {
+        count_string[0][j] = j;
+    }
+
+    for (int i = 1; i <= a_len; ++i) {
+        for (int j = 1; j <= b_len; ++j) {
+            if (A_string[i - 1] == B_string[j - 1]) {
+                count_string[i][j] = count_string[i - 1][j - 1];
+            }
+            else {
+                count_string[i][j] = min(count_string[i - 1][j], count_string[i][j - 1]) + 1;
+            }
         }
     }
 
-    cout << count_string[a_len][b_len] << endl;
+    cout << count_string[a_len][b_len];
 
     for (int i = 0; i <= a_len; i++)
     {
