cace6e9572625c4bdf7b1076e14e55d68f47d615
honey3491 <65853514+honey3491@users.noreply.github.com>
2024-07-27 16:11:07 +0900

    Title: 최장 공통 부분 수열, Time: 14ms, Memory: 4MB, Status: Passed - CodeTree

---
+++ 240727/최장 공통 부분 수열/longest-common-sequence.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string A_string, B_string;
+
+    cin >> A_string;
+    cin >> B_string;
+
+    int a_len = A_string.length();
+    int b_len = B_string.length();
+
+    int** count_string = new int* [a_len + 1];
+    for (int i = 0; i <= a_len; i++)
+    {
+        count_string[i] = new int[b_len + 1];
+    }
+
+    for (int i = 0; i <= a_len; i++)
+    {
+        for (int j = 0; j <= b_len; j++)
+        {
+            count_string[i][j] = 0;
+        }
+    }
+
+    for (int i = 1; i <= a_len; i++)
+    {
+        for (int j = 1; j <= b_len; j++)
+        {
+            if (A_string[i - 1] == B_string[j - 1])
+                count_string[i][j] = count_string[i - 1][j - 1] + 1;
+            else
+                count_string[i][j] = max(count_string[i][j - 1], count_string[i - 1][j]);
+        }
+    }
+
+    cout << count_string[a_len][b_len] << endl;
+
+    for (int i = 0; i <= a_len; i++)
+    {
+        delete[] count_string[i];
+    }
+    delete[] count_string;
+
+    return 0;
+}
