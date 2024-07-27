e9b86a3d8792e4cb89dde2d38f16446812b9a3cd
honey3491 <65853514+honey3491@users.noreply.github.com>
2024-07-27 15:27:50 +0900

    Title: 적절한 옷 고르기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240727/적절한 옷 고르기/select-proper-clothes.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    int cloth[201][200] = { 0, }, dp[200][201] = {0,};
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        int s, e, v;
+        cin >> s >> e >> v;
+        for (int j = s; j <= e; j++)
+            cloth[i][j] = v;
+    }
+
+    for (int i = 2; i <= m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int max_diff = 0;
+            if (cloth[j][i] == 0)
+                continue;
+            for (int k = 0; k < n; k++)
+            {
+                if (k == j)
+                    continue;
+                if (cloth[k][i - 1] == 0)
+                    continue;
+                max_diff = max(max_diff, dp[i-1][k] + abs(cloth[j][i] - cloth[k][i - 1]));
+            }
+            dp[i][j] = max_diff;
+        }
+    }
+    cout << *max_element(dp[m], dp[m] + n);
+    return 0;
+}
